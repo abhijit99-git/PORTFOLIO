cdf8c5642b80f00a94a810a035beeb3e61793860
Abhijit <abhijittikone0@gmail.com>
2024-12-24 22:09:38 +0530

    Latest

---
+++ scripts.js
@@ -0,0 +1,6 @@
+function loadham(){
+    const menu=document.querySelector(".links");
+    const icon=document.querySelector(".hamlogo");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
